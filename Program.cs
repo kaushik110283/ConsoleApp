49bbb692d4263acf50f245ed601dbc95da6023f8
kaushik_rishra <kaushik_rishra@yahoo.co.in>
2022-01-19 01:36:30 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
